a6c06b67f00c3e103c62be5b51ca1cecc19e65db
LANA STEPHANIE SILVERIO <lana.ssilverio@senacsp.edu.br>
2025-08-05 20:44:49 -0300

    Adiciona a programaçao do botao somar

--- calculadora.Designer.cs
+++ calculadora.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            textBox1 = new TextBox();
+            txtNumero1 = new TextBox();
             label2 = new Label();
-            textBox2 = new TextBox();
+            txtNumero2 = new TextBox();
             somar = new Button();
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
-            label3 = new Label();
+            lblResultado = new Label();
             SuspendLayout();
             // 
             // label1
@@ -48,29 +48,30 @@
             label1.TabIndex = 0;
             label1.Text = "numero 1";
             // 
-            // textBox1
+            // txtNumero1
             // 
-            textBox1.Location = new Point(16, 48);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 1;
-            textBox1.TextChanged += textBox1_TextChanged;
+            txtNumero1.Location = new Point(16, 48);
+            txtNumero1.Name = "txtNumero1";
+            txtNumero1.Size = new Size(100, 23);
+            txtNumero1.TabIndex = 1;
+            txtNumero1.TextChanged += textBox1_TextChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(240, 32);
+            label2.Location = new Point(232, 30);
             label2.Name = "label2";
-            label2.Size = new Size(38, 15);
+            label2.Size = new Size(58, 15);
             label2.TabIndex = 2;
-            label2.Text = "label2";
+            label2.Text = "numero 2";
+            label2.Click += label2_Click;
             // 
-            // textBox2
+            // txtNumero2
             // 
-            textBox2.Location = new Point(232, 48);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 3;
+            txtNumero2.Location = new Point(232, 48);
+            txtNumero2.Name = "txtNumero2";
+            txtNumero2.Size = new Size(100, 23);
+            txtNumero2.TabIndex = 3;
             // 
             // somar
             // 
@@ -89,6 +90,7 @@
             button1.TabIndex = 5;
             button1.Text = "somar";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
@@ -108,29 +110,29 @@
             button3.Text = "dividir";
             button3.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // lblResultado
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(416, 40);
-            label3.Name = "label3";
-            label3.Size = new Size(28, 32);
-            label3.TabIndex = 8;
-            label3.Text = "0";
+            lblResultado.AutoSize = true;
+            lblResultado.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblResultado.Location = new Point(416, 40);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(28, 32);
+            lblResultado.TabIndex = 8;
+            lblResultado.Text = "0";
             // 
             // calculadora
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(515, 171);
-            Controls.Add(label3);
+            Controls.Add(lblResultado);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(somar);
-            Controls.Add(textBox2);
+            Controls.Add(txtNumero2);
             Controls.Add(label2);
-            Controls.Add(textBox1);
+            Controls.Add(txtNumero1);
             Controls.Add(label1);
             Name = "calculadora";
             Text = "calculadora";
@@ -141,13 +143,13 @@
         #endregion
 
         private Label label1;
-        private TextBox textBox1;
+        private TextBox txtNumero1;
         private Label label2;
-        private TextBox textBox2;
+        private TextBox txtNumero2;
         private Button somar;
         private Button button1;
         private Button button2;
         private Button button3;
-        private Label label3;
+        private Label lblResultado;
     }
 }
--- calculadora.cs
+++ calculadora.cs
@@ -11,5 +11,20 @@ namespace caculadora
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //
+            double numero1 = Convert.ToDouble(txtNumero1.Text);
+            double numero2 = Convert.ToDouble(txtNumero2.Text);
+
+            //Realizar a soma dos numeros e exibe o resultado
+            lblResultado.Text = (numero1 + numero2).ToString();
+        }
     }
 }
